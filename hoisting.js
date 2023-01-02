7b0a843becad809e98c15874975c4ddf041d4b53
ameekaziz <ameek.aziz@gmail.com>
2023-01-02 15:25:40 +0600

    modified:   hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,15 @@
 function myf() {
-    let lang = "frnch";
+    lang = "frnch";
+    // console.log(lang)
+    var lang; // * here the var decliared before the 
     console.log(lang)
+
 }
 
 myf();
 
-var lang;
-console.log(lang)
+// var lang;
+// console.log(lang)
 
 // let lang;
 // console.log(lang)
